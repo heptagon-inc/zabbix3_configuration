387d31a866795c166abffaec6e82e8c44ea5df9c
youyo <1003ni2@gmail.com>
2016-03-27 18:28:01 +0900

    add mysql client.

--- cookbooks/mysql/client/default.rb
+++ cookbooks/mysql/client/default.rb
@@ -1,1 +1,14 @@
-package 'mysql56'
+include_cookbook 'zabbix/agent'
+
+require_relative '../../../itamae_helper.rb'
+
+%w(mysql56 mysql56-devel).each do |pkg|
+  package pkg
+end
+
+template '/var/lib/zabbix/.my.cnf' do
+  owner 'zabbix'
+  group 'zabbix'
+  mode '0640'
+  notifies :restart, "service[zabbix-agent]"
+end
